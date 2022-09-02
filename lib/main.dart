a7c634f48ac9c5ba60700dc8d957b6196aa4d027
Christopher Díaz <nealthmc@gmail.com>
2022-09-02 06:35:43 -0500

    register screen elements

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
+import 'package:stative_app/screens/register/index.dart';
 import 'package:stative_app/store/index.dart';
 import 'package:redux/redux.dart';
 import 'package:stative_app/screens/preload/index.dart';
@@ -26,12 +27,13 @@ class Stative extends StatelessWidget {
           title: 'Stative',
           theme: ThemeData(
               primarySwatch: Colors.blue,
-              primaryColor: const Color.fromRGBO(255, 0, 0, 1)
+              primaryColor: const Color.fromRGBO(255, 0, 0, 1),
+              shadowColor: const Color.fromRGBO(255, 112, 112, 1)
           ),
           initialRoute: Routes.preload,
           routes: {
             Routes.preload: (context) => const Preload(),
-            Routes.register: (context) => const Scaffold(body: SafeArea(child: Text('Register screen'))),
+            Routes.register: (context) => const Register(),
             Routes.login: (context) => const SafeArea(child: Text('Login screen')),
             Routes.initialData: (context) => const SafeArea(child: Text('initialData screen')),
             Routes.home: (context) => const SafeArea(child: Text('Home screen')),
---
+++ lib/screens/register/index.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:stative_app/store/index.dart';
+
+class RegisterModel {}
+
+class Register extends StatefulWidget {
+  const Register({ Key? key }) : super(key: key);
+  
+  @override
+  RegisterState createState() => RegisterState();
+}
+
+class RegisterState extends State<Register> {
+  @override
+  Widget build(BuildContext context) {
+    return StoreConnector<AppState, RegisterModel>(
+        builder: buildRegister,
+        converter: (store) => RegisterModel()
+    );
+  }
+
+  Widget buildRegister(BuildContext context, RegisterModel model) {
+    return Scaffold(
+        body: SafeArea(
+            child: Column(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Center(
+                    child: Image.asset(
+                      'images/icon.png',
+                      height: 150,
+                      width: 150,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Column(
+                    children: [
+                      const Text('Email', style: TextStyle(
+                        fontSize: 20,
+                      )),
+                      Center(
+                          child: SizedBox(
+                              width: 350,
+                              height: 40,
+                              child: TextField(
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(20),
+                                        borderSide: const BorderSide(
+                                          width: 0,
+                                          style: BorderStyle.none,
+                                        )
+                                    ),
+                                    filled: true,
+                                    fillColor: Theme.of(context).shadowColor,
+                                  )
+                              )
+                          )
+                      )
+                    ],
+                  )
+                ),
+              ],
+            )
+        ),
+    );
+  }
+}
